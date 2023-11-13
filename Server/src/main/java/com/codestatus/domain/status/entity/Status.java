d2beed28e98f96df1cc446e4f3e79a99c302a1bb
jjo3ys <jjo3ys@gmail.com>
2023-11-13 21:13:11 +0900

    feat: optimisticlock on status table temp

--- Server/src/main/java/com/codestatus/domain/status/entity/Status.java
+++ Server/src/main/java/com/codestatus/domain/status/entity/Status.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OptimisticLockType;
+import org.hibernate.annotations.OptimisticLocking;
 
 import javax.persistence.*;
 
@@ -12,6 +14,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @NoArgsConstructor
+//@OptimisticLocking(type = OptimisticLockType.VERSION)
 public class Status {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +37,7 @@ public class Status {
 
     @Column(nullable = false)
     private int requiredExp;
+
+//    @Version
+//    private Long version;
 }
